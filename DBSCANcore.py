93e4308cf8a1e80fa93e01d5e9a30b1f5f0bde18
yqz206@gmail.com <yqz206@gmail.com>
2020-04-23 15:27:25 +0800

    add the two main point

--- DBSCANcore.py
+++ DBSCANcore.py
@@ -7,15 +7,16 @@
 
 class myDBSCAN(object):
 
-	rms = 0.5
-	TBModel="minV3minP16_dendroCatTrunk.fit"
+    rms = 0.5
+    TBModel="minV3minP16_dendroCatTrunk.fit"
+    #tmpPath="./tmpFiles/"
+    #rawCOFITS="G2650Local30.fits"
+    #
+    pixelArea=0.25 # arcmins
 
-	tmpPath="./tmpFiles/"
+    parsecToMeter= 3.0857e16 #m
 
 
-	rawCOFITS="G2650Local30.fits"
 
-	#
-	pixelArea=0.25 # arcmins
-
-	parsecToMeter= 3.0857e16 #m
+    def __init__(self):
+        pass

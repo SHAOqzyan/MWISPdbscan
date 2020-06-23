aa4555da1cc3b25670920d90ee5ddff12a80ae2e
yqz206@gmail.com <yqz206@gmail.com>
2020-06-23 18:27:59 +0800

    aa

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
-from mpPYTHON import *
+from myPYTHON import *
 from mwispDBSCAN import MWISPDBSCAN
 
 doMWdbscan= MWISPDBSCAN()
@@ -7,8 +7,9 @@ doMWdbscan= MWISPDBSCAN()
 rawCOFITS="Q1Sub.fits"
 
 if 1:
-doMWdbscan.rawCOFITS =  coFITS
-    doMWdbscan.rmsFITS = rawCOFITS
+    doMWdbscan.rawCOFITS =  rawCOFITS
+    doMWdbscan.rmsFITS = None #you can provde rms fits if you hae one
+    doMWdbscan.averageRMS = 0.5 # if you do not have an rm fits file, use an average rms
 
     doMWdbscan.setDBSCANParameters( cutoff_sigma=2,minPts=4,connectivity= 1 )
     doMWdbscan.processPath = './'

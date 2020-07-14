7c9715ce5ab72c1615f8f2a013b84ad9d758cfb4
yqz206@gmail.com <yqz206@gmail.com>
2020-07-14 16:25:31 +0800

    remove duplicate line in exmaple.py

--- example.py
+++ example.py
@@ -25,4 +25,3 @@ if 1: #an example
 
 
     doMWdbscan.produceIndividualClouds( doMWdbscan.rawCOFITS, doMWdbscan.cleanFITSName ,doMWdbscan.cleanCatName  )
-    doMWdbscan.produceMask(doMWdbscan.rawCOFITS, doMWdbscan.cleanFITSName) # labelFITSName may be none if the computeDBSCAN() dot no process, you need to

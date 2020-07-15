a3132a57b56c24c0dbe89734fb32b8ddb83d5fab
yqz206@gmail.com <yqz206@gmail.com>
2020-07-15 16:54:01 +0800

    for server

--- example.py
+++ example.py
@@ -4,14 +4,14 @@ from mwispDBSCAN import MWISPDBSCAN
 doMWdbscan= MWISPDBSCAN()
 
 
+def pipeLine(rawCOFITS,rmsFITS=None,averageRMS=0.5):
 
-if 1: #an example
     #rawCOFITS = "../fcrao_rep_m.fits"
-    rawCOFITS = "Q1Sub.fits"
+    rawCOFITS =  rawCOFITS #"Q1Sub.fits"
 
     doMWdbscan.rawCOFITS =  rawCOFITS
-    doMWdbscan.rmsFITS = None #you can provde rms fits if you hae one
-    doMWdbscan.averageRMS = 0.5 # if you do not have an rm fits file, use an average rms
+    doMWdbscan.rmsFITS = rmsFITS #you can provde rms fits if you hae one
+    doMWdbscan.averageRMS =averageRMS # if you do not have an rm fits file, use an average rms
 
     doMWdbscan.setDBSCANParameters( cutoff_sigma=2,minPts=4,connectivity= 1 )
     doMWdbscan.processPath = './'
@@ -21,7 +21,9 @@ if 1: #an example
     doMWdbscan.produceCleanFITS()
 
 
-    doMWdbscan.produceMask(doMWdbscan.rawCOFITS, doMWdbscan.cleanFITSName ) # labelFITSName may be none if the computeDBSCAN() dot no process, you need to
+    #doMWdbscan.produceMask(doMWdbscan.rawCOFITS, doMWdbscan.cleanFITSName ) # labelFITSName may be none if the computeDBSCAN() dot no process, you need to
+
+    #doMWdbscan.produceIndividualClouds( doMWdbscan.rawCOFITS, doMWdbscan.cleanFITSName ,doMWdbscan.cleanCatName  )
 
 
-    doMWdbscan.produceIndividualClouds( doMWdbscan.rawCOFITS, doMWdbscan.cleanFITSName ,doMWdbscan.cleanCatName  )
+if 1: #an example

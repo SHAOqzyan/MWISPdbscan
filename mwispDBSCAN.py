4f06869f6c1854252807fafa4901f03e10b0efc0
yqz206@gmail.com <yqz206@gmail.com>
2020-07-21 12:44:34 +0800

    cloud cube 32

--- mwispDBSCAN.py
+++ mwispDBSCAN.py
@@ -889,8 +889,8 @@ class  MWISPDBSCAN(object):
         clusterValue1D = dataCluster[clusterIndex1D]
         Z0, Y0, X0 = clusterIndex1D
 
-        fitsZero = np.zeros_like(dataCluster,dtype=np.float64)
-        fitsZero=fitsZero+noiseMask 
+        fitsZero = np.zeros_like(dataCluster,dtype=np.float32)
+        fitsZero=fitsZero+noiseMask
          # print cloudTB.colnames
         for eachC in cloudTB:
             cloudID = eachC["_idx"]
